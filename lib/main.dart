77c93d2a5e2f9edba3978556487ffadeeaee6832
rumor1993 <rumor1993@naver.com>
2021-12-20 03:19:58 +0900

    재랜더링 시키는방법

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const MyApp());
 }
 
+var a = 1;
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -11,15 +13,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            a++;
+            print(a);
+          },
+        ),
         appBar: AppBar(
           title: Text("금호동 3가"),
           actions: [Icon(Icons.search), Icon(Icons.dehaze), Icon(Icons.notifications_none),],
         ),
-        body: ShopItem()
+        body: ShopItem(),
+        bottomNavigationBar: CustomBottomBar(),
       ),
     );
   }
 }
+
+class CustomBottomBar extends StatelessWidget {
+  const CustomBottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.call),
+            Icon(Icons.message),
+            Icon(Icons.contacts),
+          ],
+        ),
+    );
+  }
+}
+
 
 // 커스텀 위젯
 class ShopItem extends StatelessWidget {
@@ -27,9 +56,14 @@ class ShopItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Text('안녕'),
+    return ListView.builder(
+      itemCount: 3,
+      itemBuilder: (context, index){
+        return ListTile(
+          leading: Icon(Icons.account_circle),
+          title: Text(index.toString()),
+        );
+      },
     );
   }
 }
-
