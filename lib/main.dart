f01fd53d7fad5411636e56fb185a9bb94f40aa25
rumor1993 <rumor1993@naver.com>
2021-12-20 02:46:23 +0900

    커스텀 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -15,34 +15,21 @@ class MyApp extends StatelessWidget {
           title: Text("금호동 3가"),
           actions: [Icon(Icons.search), Icon(Icons.dehaze), Icon(Icons.notifications_none),],
         ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('assets/3758687.jpg', width: 150,),
-              Flexible(child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라 팝니다.'),
-                    Text('금호동 3가'),
-                    Text('7000원'),
-                    Row(
-
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4'),
-                      ],
-                    )
-                  ],
-                ),
-              ), flex: 7,)
-            ],
-          ),
-        ),
+        body: ShopItem()
       ),
     );
   }
 }
+
+// 커스텀 위젯
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text('안녕'),
+    );
+  }
+}
+
